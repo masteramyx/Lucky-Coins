c93376b7f59c01734ef93a0cce269b76e62246d2
Kyle Amyx <kyle.amyx@duethealth.com>
2019-08-14 13:47:07 -0400

    WIP: work on detail view

--- app/src/main/java/com/example/kyleamyx/luckycoins/detail/adapter/CoinDetailViewHolder.kt
+++ app/src/main/java/com/example/kyleamyx/luckycoins/detail/adapter/CoinDetailViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import com.example.kyleamyx.luckycoins.R
 import com.example.kyleamyx.luckycoins.models.CoinDetailItem
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.coin_detail_controller.view.*
 
 /**
  * Created by kyleamyx on 8/31/18.
@@ -15,6 +16,8 @@ class CoinDetailViewHolder(val itemView: View, val detailItem: CoinDetailItem) :
         Picasso.with(itemView.context).load(coin.logo)
                 .resize(50, 50)
                 .into(itemView.findViewById<ImageView>(R.id.coinHeroIv))
+
+        itemView.detailTv.text = detailItem.description
     }
 
     override fun onClick(v: View?) {
