3871721b7cf5a1d033b2b5de9a544e2dd1c634a2
Kyle Amyx <kyle.amyx@duethealth.com>
2019-11-15 00:05:38 -0500

    WIP: api key hidden; writing up README

--- app/src/main/java/com/example/kyleamyx/luckycoins/api/LuckyCoinApiService.kt
+++ app/src/main/java/com/example/kyleamyx/luckycoins/api/LuckyCoinApiService.kt
@@ -1,5 +1,6 @@
 package com.example.kyleamyx.luckycoins.api
 
+import com.example.kyleamyx.luckycoins.BuildConfig
 import com.example.kyleamyx.luckycoins.api.response.CoinDetailResponse
 import com.example.kyleamyx.luckycoins.api.response.CoinListResponse
 import io.reactivex.Observable
@@ -15,20 +16,20 @@ interface LuckyCoinApiService {
 
     @GET("/v1/cryptocurrency/listings/latest")
     fun getCoinListing(@Query("CMC_PRO_API_KEY")
-                       key: String = "7b08fc3a-c443-4d59-932d-04bfd6074005"):
+                       key: String = BuildConfig.LUCKY_COINS_API_KEY):
             Observable<CoinListResponse>
 
 
     @GET("/v1/cryptocurrency/info")
     fun getCoinDetail(@Query("CMC_PRO_API_KEY")
-                      key: String = "7b08fc3a-c443-4d59-932d-04bfd6074005",
+                      key: String = BuildConfig.LUCKY_COINS_API_KEY,
                       @Query("id")
                       coinId: String): Observable<CoinDetailResponse>
 
 
     @GET("/v1/cryptocurrency/info")
     fun getCoinListImages(@Query("CMC_PRO_API_KEY")
-                          key: String = "7b08fc3a-c443-4d59-932d-04bfd6074005",
+                          key: String = BuildConfig.LUCKY_COINS_API_KEY,
                           @Query("id")
                           coinIdList: String): Observable<CoinDetailResponse>
 
