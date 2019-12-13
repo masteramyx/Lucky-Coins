b6e2b32cc72730a352917b65689ea7a028a93f76
Kyle Amyx <kyle.amyx@duethealth.com>
2019-12-12 20:37:13 -0500

    splash screen added

---
+++ app/src/main/java/com/paperspace/kyleamyx/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.paperspace.kyleamyx
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.paperspace.kyleamyx.luckycoins.CoinMainActivity
+import com.paperspace.kyleamyx.luckycoins.R
+
+class SplashActivity : Activity() {
+
+    private val splashTime = 2000L
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handler = Handler()
+
+        handler.postDelayed(Runnable {
+            startMainActivity()
+        }, splashTime)
+
+    }
+
+    fun startMainActivity() {
+        val mainActivityIntent = Intent(applicationContext, CoinMainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
+    }
+}
+
